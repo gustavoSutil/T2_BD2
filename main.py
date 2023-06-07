1898cdb6158ea6e2154d717805214da09a926c50
Gustavo Guilherme Sutil Alberton <85875309+gustavoSutil@users.noreply.github.com>
2023-06-07 00:09:49 -0300

    #feat: criando funcionalidades faltantes
    funcionalidades referentes a issue #1

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import json
 import psycopg2
+from config import BD_CONFIGURATION as db
 
-def connect_to_database():
+def connect_to_database() -> psycopg2.connect :
     # Conectar ao banco de dados
     conn = psycopg2.connect(
-        host="localhost",
-        database="db_log",
-        user="postgres",
-        password="pgadmin"
+        host=db.config["host"],
+        database=db.config["database"],
+        user=db.config["user"],
+        password=db.config["password"]
     )
     return conn
 
@@ -22,7 +23,7 @@ def load_database(conn, table_data):
         cursor.execute(f"INSERT INTO initial (id,A,B) VALUES ({table_data['id'][id_t]},{table_data['A'][id_t]},{table_data['B'][id_t]})")
     conn.commit()
 
-def load_log(log_file):
+def load_log(log_file) -> (list,list):
     # ler o arquivo de log e retornar os valores e os processos feitos
     with open(log_file, 'r') as file:
         log_data = file.read().splitlines()
@@ -56,26 +57,80 @@ def verifyProcess(process) -> (list, list):
     return started, commited
 
 
+def check_data(started,commited,values,conn):
+    non_commited = list(set(started)-set(commited))
+
+    need_undo = []
+    need_redo = []
+    for i in range(len(values)):
+        for id_transaction in non_commited:
+            if id_transaction==values[i][0]:
+                #acha as transaçoes que nao realizaram o commit
+                need_undo.append(values[i])
+        for id_transaction in commited:
+            if id_transaction==values[i][0]:
+                #acha as transaçoes do log que realizaram o commit
+                need_redo.append(values[i])
+
+    r = redo(need_redo, conn)
+    u = undo(need_undo,conn)
 
 
+def undo(operations,conn):
+    result = []
+    #vai ver se o dado no banco esta indevidamente no banco
+    #exemplo [0:'id_transaction', 1:'id_tupla', 2:'colunm', 3'old', 4:'new']
+    cursor = conn.cursor()
+    for t in operations:
+        try:
+            cursor.execute(f"SELECT {t[2]} FROM initial WHERE id = '{t[1]}'")
+            r = cursor.fetchall()
+            if r[0][0] == t[4]:
+                cursor.execute(f"UPDATE initial SET {t[2]} = {t[3]} WHERE id = '{t[1]}';")
+                print(f"Transação {t[0]} realizou UNDO")
+        except Exception as e:
+            print("Ocorreu um erro: ", e)
+    cursor.close()
+    conn.commit()
+    return result
+
+def redo(operations, conn) -> list:
+    #vai ver se o dado esta diferente se sim atualiza no banco
+    #exemplo [0:'id_transaction', 1:'id_tupla', 2:'colunm', 3'old', 4:'new']
+    result = []
+    cursor = conn.cursor()
+    for t in operations:
+        try:
+            cursor.execute(f"SELECT {t[2]} FROM initial WHERE id = '{t[1]}'")
+            r = cursor.fetchall()
+            if r[0][0] != t[4]:
+                cursor.execute(f"UPDATE initial SET {t[2]} = {t[4]} WHERE id = '{t[1]}';")
+                print(f"Transação {t[0]} realizou REDO")
+        except Exception as e:
+            print("Ocorreu um erro: ", e)
+    cursor.close()
+    conn.commit()
+    return result
 
 def main():
     with open('metadado.json', 'r') as file:
         metadado = json.load(file) # Carregar metadados
 
 
-
     values, process = load_log('log.txt')
-    started, commited = verifyProcess(process)
+
+    started, commited = verifyProcess(process) # retorna os processos que foram comitados
 
 
     conn = connect_to_database() # Conectar ao banco de dados
 
+    # o main das operaçoes REDU/UNDU retorna o que foi feito
+    actions = check_data(started,commited,values,conn)
 
 
+    conn.close() #Fechar a conexão com o banco de dados
 
-    # Fechar a conexão com o banco de dados
-    conn.close()
+
 
 if __name__ == '__main__':
     main()
